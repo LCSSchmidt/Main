d1df36b2a61d85fbbc7b96c2ad8f3372e7f9d847
lucas ... <stit_san@hotmail.com>
2017-12-03 20:13:18 -0200

    Pagina Inicial

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,105 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
+// RSocial.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+const int BORDA_PRINCIPAL_VERTICAL = 120;
+const int BORDA_PRINCIPAL_HORIZONTAL = 60;
+const char CERCA = '#';
+const char VAZIO = '\0';
+
+struct Pagina_inicial {
+	char corpo[BORDA_PRINCIPAL_VERTICAL][BORDA_PRINCIPAL_HORIZONTAL];
+};
+
+void _preencher_corpo(Pagina_inicial &pagina, int &linha, int &coluna, int chamadas = 0, int ZERO = 0) {
+	if (linha == BORDA_PRINCIPAL_VERTICAL - 1 && coluna == BORDA_PRINCIPAL_HORIZONTAL - 1 || chamadas == 270) {
+		return;
+	}
+
+	pagina.corpo[linha][coluna] = VAZIO;
+	if (coluna != BORDA_PRINCIPAL_HORIZONTAL - 1) {
+		_preencher_corpo(pagina, linha, ++coluna, ++chamadas);
+	}
+	else {
+		_preencher_corpo(pagina, ++linha, ZERO, ++chamadas);
+	}
+}
+
+void preencher_corpo(Pagina_inicial &pagina, int linha = 0, int coluna = 0) {
+	if (linha == BORDA_PRINCIPAL_VERTICAL - 1 && coluna == BORDA_PRINCIPAL_HORIZONTAL - 1) {
+		return;
+	}
+	_preencher_corpo(pagina, linha, coluna);
+	preencher_corpo(pagina, linha, coluna);
+}
+
+void _bordas_principal(Pagina_inicial &pagina, int &auxiliar, int &passagens, int &linha, int &coluna, int chamadas = 0) {
+	if (passagens == BORDA_PRINCIPAL_VERTICAL * 2 + BORDA_PRINCIPAL_HORIZONTAL * 2 - 3 || chamadas == 270) {
+		return;
+	}
+
+	pagina.corpo[linha][coluna] = CERCA;
+
+	if (linha == BORDA_PRINCIPAL_VERTICAL - 1 && coluna != 0) {
+		_bordas_principal(pagina, auxiliar, ++passagens, linha, --coluna, ++chamadas);
+	}
+	else if (coluna == BORDA_PRINCIPAL_HORIZONTAL - 1) {
+		_bordas_principal(pagina, auxiliar, ++passagens, ++linha, coluna, ++chamadas);
+	}
+	else if (coluna == 0 && linha == auxiliar) {
+		_bordas_principal(pagina, --auxiliar, ++passagens, --linha, coluna, ++chamadas);
+	}
+	else {
+		_bordas_principal(pagina, auxiliar, ++passagens, linha, ++coluna, ++chamadas);
+	}
+}
+
+void bordas_principal(Pagina_inicial &pagina, int auxiliar, int passagens = 0, int linha = 0, int coluna = 0) {
+	if (passagens == BORDA_PRINCIPAL_VERTICAL * 2 + BORDA_PRINCIPAL_HORIZONTAL * 2 - 3) {
+		return;
+	}
+
+	_bordas_principal(pagina, auxiliar, passagens, linha, coluna);
+	bordas_principal(pagina, auxiliar, passagens, linha, coluna);
+}
+
+void _desenhar_pagina_inicial(Pagina_inicial pagina, int &linha, int &coluna, int chamadas = 0, int ZERO = 0) {
+	if (linha == BORDA_PRINCIPAL_VERTICAL - 1 && coluna == BORDA_PRINCIPAL_HORIZONTAL - 1 || chamadas == 130) {
+		cout << pagina.corpo[linha][coluna];
+		return;
+	}
+
+	cout << pagina.corpo[linha][coluna];
+
+	if (coluna != BORDA_PRINCIPAL_HORIZONTAL - 1)
+		_desenhar_pagina_inicial(pagina, linha, ++coluna, ++chamadas);
+	else {
+		cout << endl;
+		_desenhar_pagina_inicial(pagina, ++linha, ZERO, ++chamadas);
+	}
+}
+void desenhar_pagina_inicial(Pagina_inicial pagina, int linha = 0, int coluna = 0) {
+	if (linha == BORDA_PRINCIPAL_VERTICAL - 1 && coluna == BORDA_PRINCIPAL_HORIZONTAL - 1) {
+		return;
+	}
+	_desenhar_pagina_inicial(pagina, linha, coluna);
+	desenhar_pagina_inicial(pagina, linha, coluna);
+}
 
 int main()
 {
-    return 0;
+	Pagina_inicial pagina;
+	int auxiliar = BORDA_PRINCIPAL_VERTICAL - 1;
+
+	preencher_corpo(pagina);
+	bordas_principal(pagina, auxiliar);
+	desenhar_pagina_inicial(pagina);
+	system("pause");
+	return 0;
 }
 
